23c09a0503642b0eac86ec63dd2d937765f3ffff
augustine.jibunoh <jibunoh.augustine@gmail.com>
2022-03-13 00:28:20 +0100

    fix: A bug fix

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
